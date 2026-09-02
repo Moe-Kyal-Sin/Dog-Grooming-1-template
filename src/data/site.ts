7dd741c54d36d1de98eabe9b326a252ca92e083e
Moe <moekyalsin110@gmail.com>
2026-09-02 15:38:37 +0100

    Rename business from 'Boswall Dog Grooming' to 'Dog Daze Dog Grooming'

--- src/data/site.ts
+++ src/data/site.ts
@@ -12,7 +12,7 @@ import ralphImage from '../assets/images/ralph-schnauzer.png';
 
 export const site = {
   business: {
-    name: 'Boswall Dog Grooming',
+    name: 'Dog Daze Dog Grooming',
     shortName: 'GP',
     tagline: 'Good dogs. Great hair.',
     description:
